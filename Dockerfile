5e8eaa36c00d54add0fb3014fd836b99e530b527
JoaoVitorOliMendes <joaozao.oli@gmail.com>
2022-01-26 20:44:06 -0300

    docker

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM maven:3.8.4-openjdk-11-slim as BUILDER
+
+ARG VERSION=0.0.1-SNAPSHOT
+
+WORKDIR /opt/backend
+
+COPY . .
+RUN mvn clean install
+
+CMD mvn spring-boot:run
--- src/test/java/com/example/demo/PolipluzApplicationTests.java
+++ src/test/java/com/example/demo/PolipluzApplicationTests.java
@@ -3,11 +3,8 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.polipluz.main.PolipluzApplication.class)
 class PolipluzApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
